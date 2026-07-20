2dec766bc14b2f83fc5ea9aa8d0a0ac8f0857408
Tyler Tran <104945572+tylertran349@users.noreply.github.com>
2026-07-20 14:33:07 -0700

    Add: valid-tree - submission-7

---
+++ Data Structures & Algorithms/valid-tree/submission-7.py
@@ -0,0 +1,23 @@
+class Solution:
+    def validTree(self, n: int, edges: List[List[int]]) -> bool:
+        if len(edges) != n - 1:
+            return False
+        adj_list = {}
+        for i in range(n):
+            adj_list[i] = []
+        for u, v in edges:
+            adj_list[u].append(v)
+            adj_list[v].append(u)
+        queue = deque()
+        visited = set()
+        queue.append(0)
+        while queue:
+            level_size = len(queue)
+            for i in range(level_size):
+                curr_node = queue.popleft()
+                visited.add(curr_node)
+                for neighbor in adj_list[curr_node]:
+                    if neighbor not in visited:
+                        visited.add(neighbor)
+                        queue.append(neighbor)
+        return len(visited) == n

41d0d5ee24836219d0736ccd8977f8982c7b1e9f
Tyler Tran <104945572+tylertran349@users.noreply.github.com>
2026-07-21 14:30:35 -0700

    Add: meeting-schedule-ii - submission-12

---
+++ Data Structures & Algorithms/meeting-schedule-ii/submission-12.py
@@ -0,0 +1,20 @@
+"""
+Definition of Interval:
+class Interval(object):
+    def __init__(self, start, end):
+        self.start = start
+        self.end = end
+"""
+
+class Solution:
+    def minMeetingRooms(self, intervals: List[Interval]) -> int:
+        if len(intervals) == 0:
+            return 0
+        min_heap = []
+        intervals.sort(key=lambda interval: interval.start)
+        heapq.heappush(min_heap, intervals[0].end)
+        for interval in intervals[1:]:
+            if min_heap[0] <= interval.start:
+                heapq.heappop(min_heap)
+            heapq.heappush(min_heap, interval.end)
+        return len(min_heap)
